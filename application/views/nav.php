2e0d5320508d6a8e85bf7283349f4af8f7882e11
zwldqp <1464222122@qq.com>
2017-02-25 22:34:26 +0800

    project table

--- application/views/nav.php
+++ application/views/nav.php
@@ -1,19 +1,19 @@
 <div class="tpl-left-nav tpl-left-nav-hover">
     <div class="tpl-left-nav-title">
-        在线教育系统列表
+        Amaze UI 列表
     </div>
     <div class="tpl-left-nav-list">
         <ul class="tpl-left-nav-menu">
             <li class="tpl-left-nav-item">
-                <a href="welcome/index" class="nav-link">
+                <a href="welcome/t_index" class="nav-link">
                     <i class="am-icon-home"></i>
                     <span>首页</span>
                 </a>
             </li>
             <li class="tpl-left-nav-item">
-                <a href="welcome/chart" class="nav-link tpl-left-nav-link-list active">
+                <a href="welcome/t_charts" class="nav-link tpl-left-nav-link-list active">
                     <i class="am-icon-bar-chart"></i>
-                    <span>我的资源</span>
+                    <span>我的课程</span>
                     <i class="tpl-left-nav-content tpl-badge-danger">
                         12
                     </i>
@@ -23,52 +23,46 @@
             <li class="tpl-left-nav-item">
                 <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
                     <i class="am-icon-table"></i>
-                    <span>教学管理</span>
+                    <span>选课中心</span>
                     <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
                 </a>
                 <ul class="tpl-left-nav-sub-menu">
                     <li>
                         <a href="table-font-list.html">
                             <i class="am-icon-angle-right"></i>
-                            <span>学生管理</span>
+                            <span>猜你喜欢</span>
+                            <i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
                         </a>
-                        <a href="form-news.html">
+                        <a href="table-images-list.html">
                             <i class="am-icon-angle-right"></i>
-                            <span>课程管理</span>
-                        </a>
-                        <a href="form-news-list.html">
-                            <i class="am-icon-angle-right"></i>
-                            <span>作业管理</span>
-                        </a>
-                        <a href="form-news-list.html">
-                            <i class="am-icon-angle-right"></i>
-                            <span>成绩管理</span>
+                            <span>选课中心</span>
+                            <i class="tpl-left-nav-content tpl-badge-success">
+                                18
+                            </i>
                         </a>
                     </li>
                 </ul>
             </li>
-
             <li class="tpl-left-nav-item">
                 <a href="javascript:;" class="nav-link tpl-left-nav-link-list">
                     <i class="am-icon-wpforms"></i>
-                    <span>我的评价</span>
+                    <span>教师评价</span>
                     <i class="am-icon-angle-right tpl-left-nav-more-ico am-fr am-margin-right"></i>
                 </a>
                 <ul class="tpl-left-nav-sub-menu">
                     <li>
                         <a href="form-amazeui.html">
                             <i class="am-icon-angle-right"></i>
-                            <span>学生评价</span>
+                            <span>开始评价</span>
+                            <i class="am-icon-star tpl-left-nav-content-ico am-fr am-margin-right"></i>
                         </a>
-
-                        <a href="form-line.html">
+                        <a href="welcome/view_evaluation">
                             <i class="am-icon-angle-right"></i>
-                            <span>学习评价</span>
+                            <span>查看评价</span>
                         </a>
                     </li>
                 </ul>
             </li>
-
             <li class="tpl-left-nav-item">
                 <a href="welcome/introduce" class="nav-link tpl-left-nav-link-list">
                     <i class="am-icon-key"></i>
